e841c01314e5cb2aab411e9002b3b38044da33d8
kaeri <a77088554@gmail.com>
2025-02-03 20:35:17 +0800

    v1.0.2(修復圖片位置錯誤、更改main.jsx的Hashrouter)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { Routes, Route, HashRouter } from 'react-router'
 import Home from './page/Home'
 import About from './page/About'
 import Navbar from './components/Navbar'
@@ -8,13 +8,13 @@ import Footer from './components/Footer'
 
 createRoot(document.getElementById('root')).render(
 
-  <BrowserRouter>
+  <HashRouter>
     <Navbar/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
     </Routes>
     <Footer/>
-  </BrowserRouter>
+  </HashRouter>
   ,
 )
